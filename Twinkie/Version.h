c7217d173460cacb4bfa9ccbb285e57c83929271
CodyNinja1 <jailmanguy1@gmail.com>
2026-04-23 16:17:06 +0300

    v2.0.2

--- Twinkie/Version.h
+++ Twinkie/Version.h
@@ -1,13 +1,13 @@
 #pragma once
 #include "LuaEngine/include/lua.hpp"
 
-#define TwinkieVersion "v2.0.1"
+#define TwinkieVersion "v2.0.2"
 
 class Versioning
 {
 public:
 	const char* const TwinkieVer = TwinkieVersion;
-	const char* const LuaVer = LUA_VERSION;
+	const char* const LuaVer = LUA_VERSION_MAJOR "." LUA_VERSION_MINOR "." LUA_VERSION_RELEASE;
 
 	Versioning() {}
 };
